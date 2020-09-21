b8742facec259d5c8af29376f3e918481bfd090b
yude <yu@yude.moe>
2020-09-21 22:10:02 +0900

    整形

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import re
 data = "お家に、着いた。今から、愛美ちゃんと、通話をしながら、ゲームキューブで、スターフォックス、アサルトを、プレイする。"
 
 
-print("変換後の文章: \n" + re.sub('[,.。、 ]', '', data))
+print("変換対象の文章: \n" + re.sub('[,.。、 ]', '', data))
 print("\n構文解析: \n")
 mecab = MeCab.Tagger('-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd').parse(re.sub('[,.。、 ]', '', data))
 lines = mecab.split('\n')
 items = (re.split('[\t]',line) for line in lines)
 for item in items:
-    print(item)
+    if 'EOS' not in item:
+        print(item)
+
+print("\n結果: \n")
+for item in items:
+    if 'EOS' not in item:
+        print(item[0])
